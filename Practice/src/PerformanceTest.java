f5c8a65c5beb4341bb9981f26b382d0cd299e4e9
kanghee kim <86484986+KKangH@users.noreply.github.com>
2024-06-13 17:59:11 +0900

    Update PerformanceTest.java

--- Practice/src/PerformanceTest.java
+++ Practice/src/PerformanceTest.java
@@ -1,25 +1,21 @@
-
 public class PerformanceTest {
-	
     public static void main(String[] args) {
-    	
         Book[] books = new Book[1000000];
-        
         for (int i = 0; i < books.length; i++) {
             books[i] = new Book(i, "Book " + i, "Author " + i, 2000 + i);
         }
-        
         Library library = new Library(books);
 
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
         library.search(999999);
-        
-        long endTime = System.currentTimeMillis();
-        System.out.println("Linear search time: " + (endTime - startTime) + "ms");
+        long endTime = System.nanoTime();
+        double elapsedTimeLinear = (endTime - startTime) / 1_000_000.0; // 변환하여 소수점 단위로 나타냄
+        System.out.println("Linear search time: " + elapsedTimeLinear + "ms");
 
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         library.search_bs(999999);
-        endTime = System.currentTimeMillis();
-        System.out.println("Binary search time: " + (endTime - startTime) + "ms");
+        endTime = System.nanoTime();
+        double elapsedTimeBinary = (endTime - startTime) / 1_000_000.0; // 변환하여 소수점 단위로 나타냄
+        System.out.println("Binary search time: " + elapsedTimeBinary + "ms");
     }
 }
